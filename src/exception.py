6105a14f21e7e6cd0e2afdb18b7fbebbba8f56ac
Aasif-786 <aasifkhan47377@gmail.com>
2024-12-08 17:24:09 +0530

    eda and problem statement

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -20,9 +20,9 @@ class CustomException(Exception):
     
 '''if __name__=="__main__":
     try:
-      a = 12 / 5
+      a = 1/0
     except Exception as e:
-        logging.info("divisible")
+        logging.info(" not divisible")
         raise CustomException(e, sys)'''
 
 
